de25cfa6c3a27a7b92db08033da8dcca534d6a83
yufang <yufang@retaileye.ai>
2021-08-03 09:40:19 +0800

    update windi config

--- vite-demo/windi.config.js
+++ vite-demo/windi.config.js
@@ -2,17 +2,19 @@ import { defineConfig } from 'windicss/helpers'
 
 module.exports = {
   theme: {
-    colors: {
-      red: {
-        medium: '#e53e3e',
-        dark: '#742a2a'
-      },
-      blue: {
-        medium: '#3182ce',
-        dark: '#2a4365'
-      },
-      'th-primary': 'var(--primary)',
-      'th-secondary': 'var(--secondary)'
+    extend:{
+      colors: {
+        red: {
+          medium: '#e53e3e',
+          dark: '#742a2a'
+        },
+        blue: {
+          medium: '#3182ce',
+          dark: '#2a4365'
+        },
+        'th-primary': 'var(--primary)',
+        'th-secondary': 'var(--secondary)'
+      }
     }
   }
 }
