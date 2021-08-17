5636f98803ed70a509f51db0489bc8e69c5e63a5
louis.yu <yf_hello@163.com>
2021-08-17 21:58:30 +0800

    bug fix

--- climb/bootstrap-custom/main.js
+++ climb/bootstrap-custom/main.js
@@ -2,5 +2,5 @@ import './style.scss'
 
 import './cheatsheet.css'
 
-import './node_modules/bootstrap/dist/js/bootstrap.esm.js'
+import './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import './cheatsheet.js'
