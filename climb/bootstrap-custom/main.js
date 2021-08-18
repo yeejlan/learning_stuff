bc7e5bd05fe2cb6958a983cea4db042dfecc4416
louis.yu <yf_hello@163.com>
2021-08-18 20:55:24 +0800

    bug fix

--- climb/bootstrap-custom/main.js
+++ climb/bootstrap-custom/main.js
@@ -2,5 +2,5 @@ import './style.scss'
 
 import './cheatsheet.css'
 
-import './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
-import './cheatsheet.js'
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
