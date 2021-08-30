bf769064328f6036fab8db76777de030ec2dd2ca
yufang <yufang@retaileye.ai>
2021-08-30 15:39:03 +0800

    add test

---
+++ dl/mnist/test.py
@@ -0,0 +1,42 @@
+import torch
+from torch.utils.data import DataLoader
+import torchvision
+import torch.nn.functional as F
+from net import Net
+
+batch_size_test = 1000
+
+network = Net()
+
+network_state_dict = torch.load('model_0.pth')
+network.load_state_dict(network_state_dict)
+
+
+test_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./data/', train=False, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_test, shuffle=False)
+
+
+def test():
+  network.eval()
+  test_loss = 0
+  correct = 0
+  with torch.no_grad():
+    for data, target in test_loader:
+      output = network(data)
+      torch.log(output)
+      test_loss += F.nll_loss(output, target, size_average=False).item()
+      pred = output.data.max(1, keepdim=True)[1]
+      correct += pred.eq(target.data.view_as(pred)).sum()
+  test_loss /= len(test_loader.dataset)
+  print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+    test_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
+
+
+test()  
