620acf4477878af50fc27370353aefe2a8942511
yufang <yufang@retaileye.ai>
2021-09-17 13:50:38 +0800

    main

--- dl/pet/main.py
+++ dl/pet/main.py
@@ -5,29 +5,21 @@ import os
 
 base_dataset = os.getenv('DETECTRON2_DATASETS')
 base_dir=os.path.join(base_dataset, 'pet')
-register_pet_voc('pet_voc_cat_2_trainval',base_dir,'trainval','2007')
+
+dataset = 'pet_voc_cat_2_trainval'
+register_pet_voc(dataset ,base_dir,'trainval','2007')
 #register_pet_voc('pet_voc_cat_2_test',base_dir,'test','2007', False)
 
+meta = MetadataCatalog.get(dataset)
+dataset_dicts = DatasetCatalog.get(dataset)
+print("data loaded, total={}".format(len(dataset_dicts)))
+print(dataset_dicts[0])
 
-trainval_data = DatasetCatalog.get("pet_voc_cat_2_trainval")
-print("data loaded, trainval total={}".format(len(trainval_data)))
-print(trainval_data[0])
-
-#test_data = DatasetCatalog.get("pet_voc_cat_2_test")
-#print("data loaded, test total={}".format(len(test_data)))
-#print(test_data)
-
-# register_pet_voc('pet37',base_dir,'trainval','2007')
-# trainval_data = DatasetCatalog.get("pet37")
-# print("data loaded, trainval total={}".format(len(trainval_data)))
-# print(trainval_data[2289])
 
 import random
 import cv2
 from detectron2.utils.visualizer import Visualizer
 
-meta = MetadataCatalog.get('pet_voc_cat_2_trainval')
-dataset_dicts = trainval_data
 for d in random.sample(dataset_dicts, 10):
 	img = cv2.imread(d["file_name"])
 	visualizer = Visualizer(img[:, :, ::-1], metadata=meta)
