2e6006b32783613f083319415617c30afa144bd1
yufang <yufang@retaileye.ai>
2021-09-23 16:47:32 +0800

    small changes

--- temp_stuff/usage.php
+++ temp_stuff/usage.php
@@ -9,6 +9,16 @@ function foo(){
         $tr = new TinyResult('bad result', $e);
         print_r($tr->getCause());
     }
+
+    echo '~~~~~~',PHP_EOL;
+    try{
+        $tr = new TinyResult('db error', null);
+        $tr->data();
+    }catch(\Exception $e){
+        $tr = new TinyResult('got some error', $e);
+        print_r($tr->getCause());
+    }
+    echo '~~~~~~',PHP_EOL;
     $tr = new TinyResult('error chain', 'my error description');
     $tr2 = new TinyResult('error chain 2',  $tr);
     print_r($tr2->getCause());
@@ -17,6 +27,7 @@ function foo(){
 function bar(){
     $tr = new TinyResult('my error', 'my error description');
     print_r($tr->getCause());
+    echo '~~~~~~',PHP_EOL;
     throw new \Exception('somthing happened');
 }
 
