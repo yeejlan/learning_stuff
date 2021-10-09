2992961edc1a5e686dcfab94c753701b95facdf9
yufang <yufang@retaileye.ai>
2021-10-09 17:47:21 +0800

    change sidebar

--- saga-app/src/view/common/sidebar.jsx
+++ saga-app/src/view/common/sidebar.jsx
@@ -16,8 +16,20 @@ const sidebar_menus = [
       { path: "/getting-started/javaScript", name: "JavaScript"},
     ]},
   
-  { path: "/dashboard", name: "Dashboard"},
-  { path: "/counter", name: "Counter"},
+  { path: "/dashboard", name: "Dashboard", submenu: [
+      { path: "/getting-started/introduction", name: "Introduction"},
+      { path: "/getting-started/download", name: "Download"},
+      { path: "/getting-started/contents", name: "Contents"},
+      { path: "/getting-started/browsers-devices", name: "Browsers & devices"},
+      { path: "/getting-started/javaScript", name: "JavaScript"},
+    ]},
+  { path: "/counter", name: "Counter", submenu: [
+      { path: "/getting-started/introduction", name: "Introduction"},
+      { path: "/getting-started/download", name: "Download"},
+      { path: "/getting-started/contents", name: "Contents"},
+      { path: "/getting-started/browsers-devices", name: "Browsers & devices"},
+      { path: "/getting-started/javaScript", name: "JavaScript"},
+    ]},
 ]
 
 function SideBarContent(props) {
@@ -40,16 +52,16 @@ function Sidebar(props) {
           'border-end', 'shadow-sm')}>
             {sidebar_menus.map((one, i) => (
               <div key={i} class="sidebar_item sidebar_item_open ">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="15" height="17" 
-                  fill="#175d96" className="sidebar_item_toggle">
+                <svg className="sidebar_item_toggle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="15" height="17" 
+                  fill="#175d96">
                 <path d="M9.2 4.5l4.5 4.7c.2.2.3.5.3.8s-.1.6-.3.8l-4.5 4.7c-.5.4-1.2.4-1.7 0-.4-.5-.4-1 0-1.6l3.8-3.9-3.8-4c-.4-.4-.4-1 0-1.5.5-.4 1.2-.4 1.7 0z"></path>
                 </svg>
-                <Link ariaCurrent="page" className="sidebar_item_title active" to={one.path}>{one.name}</Link>
+                <Link ariaCurrent="page" className="sidebar_item_title text-dark font-monospace" to={one.path}>{one.name}</Link>
                 {one.submenu != undefined && 
-                <ul>
+                <ul className="sidebar_item_parent">
                   {one['submenu'].map((subone, j) => (
-                  <li key={j}>
-                    <Link ariaCurrent="page" className="active" to={subone.path}>{subone.name}</Link>
+                  <li key={j} className="sidebar_item_child">
+                    <Link ariaCurrent="page" className="text-dark font-monospace" to={subone.path}>{subone.name}</Link>
                   </li>
                   ))}
                 </ul>
