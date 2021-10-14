00dcbf3f83e0d0d79ca0fd38956e4d4841f084b3
louis.yu <yf_hello@163.com>
2021-10-14 22:00:48 +0800

    welcome page

--- saga-app/src/view/common/welcome.jsx
+++ saga-app/src/view/common/welcome.jsx
@@ -4,7 +4,7 @@ function Welcome() {
   return (
 <>
   <h5 className='p-4'>Welcome.</h5>
-  <span className='p-4 text-secondary'>You might want talk to your system administrator if this is the only page you can see.</span>
+  <span className='p-4 text-secondary'>You might want talk to system administrator if this is the only page you can see.</span>
 </>
   );
 }
