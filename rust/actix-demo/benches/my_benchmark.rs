8cb670004764c0fd3c2b9d72dc654af1b8e3cccd
Louis Yu <louis.yu@flashwire.com>
2023-07-19 13:04:27 +0800

    benchmark

--- rust/actix-demo/benches/my_benchmark.rs
+++ rust/actix-demo/benches/my_benchmark.rs
@@ -1,15 +1,16 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use hello::ext::err_code;
 
-pub fn get_map_with_cache_benchmark(c: &mut Criterion) {
-    c.bench_function("user status get_map_with_cache", 
-        |b| b.iter(||  err_code::get_map_once() ));
-}
+// pub fn get_map_with_cache_benchmark(c: &mut Criterion) {
+//     c.bench_function("user status get_map_with_cache", 
+//         |b| b.iter(||  err_code::get_map_once() ));
+// }
 
 pub fn get_map_benchmark(c: &mut Criterion) {
     c.bench_function("user status get_map", 
         |b| b.iter(||  err_code::get_map() ));
 }
 
-criterion_group!(benches, get_map_with_cache_benchmark, get_map_benchmark);
+// criterion_group!(benches, get_map_with_cache_benchmark, get_map_benchmark);
+criterion_group!(benches, get_map_benchmark);
 criterion_main!(benches);
