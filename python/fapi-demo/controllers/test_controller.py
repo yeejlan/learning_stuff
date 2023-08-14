cd53c63d68a3a3ae7f7bfce9280fa01cdf3c174e
Louis Yu <louis.yu@flashwire.com>
2023-08-14 10:51:44 +0800

    add api doc

--- python/fapi-demo/controllers/test_controller.py
+++ python/fapi-demo/controllers/test_controller.py
@@ -1,5 +1,7 @@
 from typing import Generator
 from fastapi import APIRouter, Depends
+from fastapi.params import Body
+from pydantic import BaseModel, Field
 from reply import Reply
 from exception import UserException
 
@@ -46,3 +48,47 @@ def test_unknown_error():
     ```
     """    
     raise Exception("this is unknown error")
+
+class MyPayload(BaseModel):
+    name: str = Field(None, min_length=3, max_length=20)
+    price: float = Field(..., ge=1.05, le=100.67) 
+    qty: int
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "my name",
+                    "price": 3.05,
+                    "qty": 10
+                }
+            ]
+        }
+    }    
+
+@router.post("/json")
+def test_json_params_validation(p: MyPayload):
+    """
+    Test unknown error.
+
+    ```
+    name: optional, min_length=3, max_length=20
+    price: ge=1.05, le=100.67
+    qty: int
+    ```
+    
+    Output
+    ```
+    {
+        "code": 0,
+        "message": "success",
+        "reason": "success",
+        "data": {
+            "name": "my name",
+            "price": 3.05,
+            "qty": 10
+        }
+    }
+    ```
+    """        
+    return Reply.success(p.model_dump())
