692a6ea868171590351807a21fb09d6a271e602a
Louis Yu <louis.yu@flashwire.com>
2023-08-19 17:16:58 +0800

    config logger

--- python/fapi-demo/log.py
+++ python/fapi-demo/log.py
@@ -1,16 +1,25 @@
 from datetime import datetime
 import logging
+from logging.handlers import TimedRotatingFileHandler
 from contextvars import ContextVar
+
 
 request_id = ContextVar('request_id', default='0000')  
 channel = ContextVar('channel', default='app')
 
-
-FORMAT = '%(iso8601)s [%(channel)s] [%(levelname)s] %(message)s {request_id=%(request_id)s}'
-logging.basicConfig(level=logging.DEBUG, format=FORMAT)
-
+CUSTOM_FORMAT = '%(iso8601)s [%(channel)s] [%(levelname)s] %(message)s {request_id=%(request_id)s}'
 
 class CustomLogger(logging.Logger):
+    def __init__(self, name):
+        super().__init__(name) 
+        formatter = logging.Formatter(CUSTOM_FORMAT)
+        handler = logging.StreamHandler() 
+        handler.setFormatter(formatter)
+        self.addHandler(handler)
+        #add file handler
+        file_handler = TimedRotatingFileHandler('storage/logs/uvicorn.log', when='midnight', delay=True)
+        file_handler.setFormatter(formatter)
+        self.addHandler(file_handler)
 
     def _log(self, level, msg, args, exc_info=None, extra=None):
         if extra is None:
@@ -23,7 +32,7 @@ class CustomLogger(logging.Logger):
 
 logging.setLoggerClass(CustomLogger)
 
-__logger= logging.getLogger(__name__)
+__logger= logging.getLogger('app.logger')
 
 def get_logger(channel_name: str = 'app') -> logging.Logger:
     channel.set(channel_name)
--- python/fapi-demo/run.sh
+++ python/fapi-demo/run.sh
@@ -1,1 +1,3 @@
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-config uvicorn.json --no-access-log
+
+# uvicorn main:app --host 0.0.0.0 --port 8000 --reload --no-access-log --use-colors
