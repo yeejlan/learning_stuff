956a8ac0e7185685f1585fda83fea321f260a7af
Louis Yu <louis.yu@flashwire.com>
2023-08-20 08:00:07 +0800

    add install packages

--- python/fapi-demo/run.sh
+++ python/fapi-demo/run.sh
@@ -3,3 +3,6 @@ uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-config uvicorn.json -
 # uvicorn main:app --host 0.0.0.0 --port 8000 --reload --no-access-log --use-colors
 
 # gunicorn -k uvicorn.workers.UvicornWorker main:app -c unicorn.conf.py
+
+# install packages
+# pip3 install -r /app/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
