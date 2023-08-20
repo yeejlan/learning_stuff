d2f3a746e695a617d4d5050fe86ca1e215a4a381
Louis Yu <louis.yu@flashwire.com>
2023-08-20 08:08:31 +0800

    add SIGHUP

--- python/fapi-demo/run.sh
+++ python/fapi-demo/run.sh
@@ -6,3 +6,7 @@ uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-config uvicorn.json -
 
 # install packages
 # pip3 install -r /app/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+
+# reload and rotate daily log for gunicorn
+# kill -SIGHUP $(pidof gunicorn_manager_porcess)
