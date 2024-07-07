73644fb3c5dd1977a274fad726fda6c054a5e337
Louis Yu <louis.yu@flashwire.com>
2024-07-07 19:27:23 +0800

    request_context

--- python/fapi-demo/core/request_context.py
+++ python/fapi-demo/core/request_context.py
@@ -20,7 +20,7 @@ async def requestContextMiddleware(request: Request, call_next):
         request_context_var.reset(token)
 
 
-def get_request_context() -> dict[str, Any]:
+def getRequestContext() -> dict[str, Any]:
     return request_context_var.get()
 
 def getContext(key: str, default=None):
@@ -33,14 +33,14 @@ def setContext(key: str, value: Any):
     request_context_var.set(ctx)
 
 if __name__ == "__main__":
-    ctx = get_request_context()
+    ctx = getRequestContext()
     print(ctx)
 
     setContext('uid', 101)
     setContext('req_id', 9001)
-    ctx = get_request_context()
+    ctx = getRequestContext()
     print(ctx)
 
     setContext('orderId', 123)
-    ctx = get_request_context()
+    ctx = getRequestContext()
     print(ctx)
