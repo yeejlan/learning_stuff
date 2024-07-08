f752c43fe778b08c3e73e38e1e7aca7f1416a6e8
Louis Yu <louis.yu@flashwire.com>
2024-07-08 17:35:44 +0800

    remove env init

--- python/fapi-demo/main.py
+++ python/fapi-demo/main.py
@@ -1,10 +1,6 @@
-from dotenv import load_dotenv
 from fastapi.staticfiles import StaticFiles
 from controllers import router
 from bootstrap import app
-
-# Load env file
-load_dotenv()
 
 app.include_router(router)
 app.mount("/public", StaticFiles(directory="public"), name="public")
