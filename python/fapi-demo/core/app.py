fb87280b081ed564085b70645c943537d5b0e457
Louis Yu <louis.yu@flashwire.com>
2024-07-11 11:26:54 +0800

    add servers

--- python/fapi-demo/core/app.py
+++ python/fapi-demo/core/app.py
@@ -10,23 +10,24 @@ AppContext.init()
 config = AppContext.getConfig()
 isDebug = AppContext.isDebugMode()
 
+servers=[
+            {"url": "/", "description": "Local"},
+            {"url": "https://stag.example.com", "description": "Staging environment"},
+            {"url": "https://prod.example.com", "description": "Production environment"},
+];
+
 appOptions = {
     "host": config.get('LISTEN_HOST', "0.0.0.0"),
     "port": config.getInt('LISTEN_PORT', 5000),
     "title": config.get('APP_NAME', "My FastApi Demo"),
     "version": config.get('APP_VERSION', "1.0.0"),
-    "redoc_url": None  # disable ReDoc
+    "redoc_url": None,  # disable ReDoc
+    "servers": servers,
 }
 if not isDebug:
     appOptions['docs_url'] = None
 
-app = FastAPI(
-servers=[
-            {"url": "/", "description": "Local"},
-            {"url": "https://stag.example.com", "description": "Staging environment"},
-            {"url": "https://prod.example.com", "description": "Production environment"},
-        ],
-    **appOptions)
+app = FastAPI(**appOptions)
 
 def getApp() -> FastAPI:
     return app
