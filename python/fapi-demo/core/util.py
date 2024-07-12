84ab642e7289ccbdb1a4910b6a2c2c5ba3d34cae
Louis Yu <louis.yu@flashwire.com>
2024-07-12 12:19:54 +0800

    run_sync

--- python/fapi-demo/core/util.py
+++ python/fapi-demo/core/util.py
@@ -1,4 +1,8 @@
+import asyncio
 from datetime import datetime, timezone
+from typing import Any, Callable, TypeVar
+
+T = TypeVar('T')
 
 def now_with_timezone(tz: timezone = timezone.utc) -> datetime:
     now = datetime.now(tz)
@@ -10,3 +14,8 @@ def now_as_iso8601(tz: timezone = timezone.utc, with_microseconds: bool = True) 
         now = now.replace(microsecond=0)
     
     return now.isoformat()
+
+async def run_sync(func: Callable[..., T], *args: Any, **kwargs: Any) -> T:
+    # Execute synchronous function in a thread pool
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, lambda: func(*args, **kwargs))
