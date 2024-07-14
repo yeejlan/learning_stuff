8022fc7e2d9abcdfbb004d6e21b5e86e29222c10
Louis Yu <louis.yu@flashwire.com>
2024-07-15 00:38:00 +0800

    unicorn config

--- python/fapi-demo/unicorn.conf.py
+++ python/fapi-demo/unicorn.conf.py
@@ -2,9 +2,8 @@ import multiprocessing
 from core.logger import getDailyLogName
 
 bind = "0.0.0.0:8000"
-# workers = multiprocessing.cpu_count() * 2 + 1
-workers = 1
-reload = True
+workers = multiprocessing.cpu_count() * 2 + 1
+reload = False
 loglevel = 'info'
 errorlog = getDailyLogName('gunicorn')
 
