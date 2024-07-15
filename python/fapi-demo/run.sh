c26cca3f962bd1e85c775bbb9b432c5cb80fe1f1
Louis Yu <louis.yu@flashwire.com>
2024-07-15 11:34:20 +0800

    run with gunicorn

--- python/fapi-demo/run.sh
+++ python/fapi-demo/run.sh
@@ -1,9 +1,9 @@
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload --no-access-log
+#uvicorn main:app --host 0.0.0.0 --port 8000 --reload --no-access-log
 
 #uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-config uvicorn.json --no-access-log
 
 # simulate prod runing
-# gunicorn -k uvicorn.workers.UvicornWorker main:app -c unicorn.conf.py
+gunicorn -k uvicorn.workers.UvicornWorker main:app -c unicorn.conf.py
 
 # install packages
 # pip3 install -r /app/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
--- python/fapi-demo/unicorn.conf.py
+++ python/fapi-demo/unicorn.conf.py
@@ -3,7 +3,7 @@ from core.logger import getDailyLogName
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-reload = False
+reload = True
 loglevel = 'info'
 errorlog = getDailyLogName('gunicorn')
 timeout = 60
