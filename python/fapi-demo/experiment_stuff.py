d0a73d94c908406170147654368549a71337e127
Louis Yu <louis.yu@flashwire.com>
2024-07-16 13:13:50 +0800

    add experiment_stuff

---
+++ python/fapi-demo/experiment_stuff.py
@@ -0,0 +1,21 @@
+
+from datetime import datetime
+from typing import Tuple
+
+from pydantic import BaseModel
+
+class Delivery(BaseModel):
+    timestamp: datetime
+    dimensions: Tuple[int, int]
+
+
+m = Delivery(timestamp='2020-01-02T03:04:05Z', dimensions=['10', '20'])
+print(repr(m.timestamp))
+#> datetime.datetime(2020, 1, 2, 3, 4, 5, tzinfo=TzInfo(UTC))
+print(m.dimensions)
+obj = m.model_dump()
+print(obj)
+print(m.__dict__)
+
+m2 = Delivery(**obj)
+print(m2.timestamp)
