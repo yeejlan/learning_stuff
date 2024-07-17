4c01f888da5c1b4e956aaf5a3232d7a146f07d28
Louis Yu <louis.yu@flashwire.com>
2024-07-17 17:33:42 +0800

    add cors

--- python/fapi-demo/bootstrap.py
+++ python/fapi-demo/bootstrap.py
@@ -5,6 +5,7 @@ from core.auth_context import AuthContextAsgiMiddleware
 from core.cache import RefreshCacheAsgiMiddleware
 from core.request_context import RequestContextAsgiMiddleware
 from core.exception import ExceptionHandlerAsgiMiddleware, validation_exception_handler
+from fastapi.middleware.cors import CORSMiddleware
 
 app = getApp()
 
@@ -15,7 +16,17 @@ app.add_middleware(ExceptionHandlerAsgiMiddleware)
 app.add_middleware(AuthContextAsgiMiddleware)
 app.add_middleware(RequestContextAsgiMiddleware)
 app.add_middleware(RefreshCacheAsgiMiddleware)
- 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization", 
+        "X-Requested-With", "Accept", "Origin",
+        "Access-Control-Request-Headers", "Access-Control-Request-Method",
+        "DNT", "X-CSRF-Token", "X-XSRF-TOKEN", "Cookie", 
+    ],
+)
 
 @app.exception_handler(RequestValidationError)
 async def request_validation_exception_handler(request, ex):
