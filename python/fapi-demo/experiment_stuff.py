201c1c7afbfbf0d22909261688d55c9c5e482184
Louis Yu <louis.yu@flashwire.com>
2024-07-18 17:05:45 +0800

    use arrow

--- python/fapi-demo/experiment_stuff.py
+++ python/fapi-demo/experiment_stuff.py
@@ -1,25 +1,15 @@
+import arrow
+
+now = arrow.now()
+
+arrow_obj = arrow.Arrow(2023, 1, 1, 12, 0, 0)
+arrow_obj = now
+print(now)
 
 
-from datetime import datetime
-from pydantic import Field
-from core.core_model import CoreModel
-from core.time_util import fallback_to_local_timezone, now, now_as_iso8601, now_as_mysql_datetime, now_with_timezone
+datetime_obj = arrow_obj._datetime
+a = arrow.Arrow.fromdatetime(datetime_obj)
 
+print(datetime_obj)  
+print(a)
 
-class UserModel(CoreModel):
-    id: int
-    name: str
-    email: str|None
-    password: str = Field(exclude=True)
-
-
-u = UserModel(id=1, name='nana', password = '1111', email='cc@dd')
-
-print(u)
-print(now_with_timezone())
-print(now_as_iso8601())
-
-n = datetime.now()
-nn = fallback_to_local_timezone(n)
-print(now_as_mysql_datetime())
-print(nn)
